547375aa0e0eb4ee8f395407ef3b31e7fe2f0a46
JTKnox91 <jtknox91@gmail.com>
2022-12-12 16:55:38 -0700

    Day 06

---
+++ day-06/two.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+class BufferChecker {
+  final List<String> input;
+
+  // Index of char to add on `next`.
+  int nextIndex;
+
+  // Index of char to remove on `next`.
+  int lastIndex;
+
+  final Map<String, int> charCounts = {};
+
+  int duplicateCount = 0;
+
+  BufferChecker(this.input, int size) : nextIndex = size, lastIndex = 0 {
+    for (var i = lastIndex; i < nextIndex; i++) {
+      final nextChar = input[i];
+      if (charCounts.containsKey(nextChar)) {
+        charCounts[nextChar] = charCounts[nextChar]! + 1;
+        duplicateCount++;
+      } else {
+        charCounts[nextChar] = 1;
+      }
+    }
+  }
+
+  void next() {
+    if (lastIndex >= input.length) {
+      throw ArgumentError('Input length exceeded');
+    }
+
+    final lastChar = input[lastIndex++];
+    final nextChar = input[nextIndex++];
+    if (charCounts[lastChar]! > 1) {
+      charCounts[lastChar] = charCounts[lastChar]! - 1;
+      duplicateCount--;
+    } else {
+      charCounts.remove(lastChar);
+    }
+    if (charCounts.containsKey(nextChar)) {
+      charCounts[nextChar] = charCounts[nextChar]! + 1;
+      duplicateCount++;
+    } else {
+      charCounts[nextChar] = 1;
+    }
+  }
+}
+
+void main() {
+  final File inputFile = new File('one.input.txt');
+  final input = inputFile.readAsLinesSync()[0].split('');
+
+  final bufferChecker = BufferChecker(input, 14);
+
+  while (bufferChecker.duplicateCount > 0) {
+    bufferChecker.next();
+  }
+
+  print(bufferChecker.nextIndex);
+}
